e0370f5b69bfd1e8accae4a8bb9e8dee5de895dd
Guido Pablo Marsico <gmarsico@navent.com>
2024-08-18 09:58:12 -0300

    Creo objeto Helper para el listado

---
+++ src/main/java/com/mutation/testing/demo/listado/ListadoHelper.java
@@ -0,0 +1,19 @@
+package com.mutation.testing.demo.listado;
+
+import com.mutation.testing.demo.model.Publicacion;
+
+import java.util.List;
+
+public class ListadoHelper {
+
+
+    public static List<Publicacion> getEmprendimientos(List<Publicacion> publicacionesRaw){
+        return publicacionesRaw;
+    }
+
+    public static List<Publicacion> getUnidades(List<Publicacion> publicacionesRaw){
+        return publicacionesRaw;
+    }
+
+
+}
---
+++ src/test/java/com/mutation/testing/demo/listado/ListadoHelperTest.java
@@ -0,0 +1,16 @@
+package com.mutation.testing.demo.listado;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListadoHelperTest {
+
+    @Test
+    void getEmprendimientos() {
+    }
+
+    @Test
+    void getUnidades() {
+    }
+}
