cf64d650e472596d49f7ac327dab955bfcc5bf68
Guido Pablo Marsico <gmarsico@navent.com>
2024-08-17 23:30:28 -0300

    Elimino metodo default sort, queda responsabilidad en service

--- src/main/java/com/mutation/testing/demo/listado/Listado.java
+++ src/main/java/com/mutation/testing/demo/listado/Listado.java
@@ -11,10 +11,6 @@ public interface Listado {
 
     List<Card> getListado(DataSource ds);
 
-    default List<Card> sortListado(List<Card> listadoToSort){
-        return listadoToSort;
-    }
-
     default Card armarCard(Publicacion publicacion,String tipoPublicacion){
         String tipoPropiedad = Util.getTipoPropiedadById(publicacion.idTipoDePropiedad()).nombre;
         String nivel = Util.getNivelById(publicacion.idNivel()).nombre;
